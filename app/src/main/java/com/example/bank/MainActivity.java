8d4b95659d1cb0e30a0b928f1dd2c24d571e8974
Lucas Silva <lucass.silva@me.com>
2020-08-06 20:56:12 -0300

    Alterando testes

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.bank;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,24 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Usuario user = new Usuario("lucasdd","lucasdd@gmail.com","123456");
-         System.out.println(user.nome);
-
+        Usuario user = new Usuario("lucasdd", "lucasdd@gmail.com", "123456");
+        System.out.println(user.nome);
         System.out.println(user.email);
-
         System.out.println(user.senha);
-
-
-        Usuario user2 = new Usuario("fulano","fulano32@gmail.com","1247@");
-
-        System.out.println(user2.nome);
-        System.out.println(user2.email);
-        System.out.println(user2.senha);
-
-        Usuario usuarioLucas  = new Usuario("fabricio","fabri10@gmial.com","24584");
-        System.out.println(usuarioLucas.nome);
     }
-
-
 }
