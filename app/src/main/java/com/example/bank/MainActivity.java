6d0af008e9cb16b2ef66c5a948fb2503b4626fe9
Lucas Silva <lucass.silva@me.com>
2020-08-06 21:12:17 -0300

    Outro jeito de commitar

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Usuario user = new Usuario("Bruno", "lucasdd@gmail.com", "123456");
         System.out.println(user.nome);
         System.out.println(user.email);
