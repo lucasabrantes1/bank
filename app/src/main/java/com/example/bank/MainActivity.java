75a645823940c0cd4ee01eb5c0ef01a42856ed22
Lucas Silva <lucass.silva@me.com>
2020-08-06 21:10:52 -0300

    Resolvendo conflito

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -10,11 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-        Usuario user = new Usuario("Pedro", "lucasdd@gmail.com", "123456");
-=======
         Usuario user = new Usuario("Bruno", "lucasdd@gmail.com", "123456");
->>>>>>> e916ed0aad9e415004352601a9f6c4be2870d44b
         System.out.println(user.nome);
         System.out.println(user.email);
         System.out.println(user.senha);
